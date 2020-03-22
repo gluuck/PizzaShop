2d3744c6c9cba6e3431e7b0d305ab1cacdf294a8
Vladimir Ostapenko <ostapenkov74@gmail.com>
2020-03-22 15:44:36 +0500

    add Rakefile, change gemfile,add class

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/activerecord"
 
+set :database , "sqlite3:pizzashop.db"
 
+class Product < ActiveRecord::Base
+#	before_filter :find_model
+
+	
+
+#	private
+#	def find_model
+#		@model = Model.find(params[:id]) if params[:id]
+#	end
+end
 
 get '/' do
 	erb :index
