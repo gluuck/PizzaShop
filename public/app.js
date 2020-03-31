5200a704c28dba2385667d9e214cd96f21eb50b2
Vladimir Ostapenko <ostapenkov74@gmail.com>
2020-03-31 11:39:43 +0500

    add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -11,4 +11,18 @@ function add_to_card(id) {
 	var x= window.localStorage.getItem(key) ;
 	x=x*1+1;
 	window.localStorage.setItem(key,x);
+	console.log ('Item in your cart: '+cart_get_number_of_items());
+}
+function cart_get_number_of_items() 
+{
+	var cnt = 0;
+	for (var i = 0 ;i< window.localStorage.length ;  i++) {
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		if (key.indexOf('product_')==0) 
+		{
+			cnt= cnt+value*1;
+		}
+	}
+	return cnt;
 }
